25743d5600ab09c38a894c9cfecf2f943591fb87
Harsha <harshakokel@gmail.com>
2018-05-01 19:14:23 -0500

    New strategy of reading file
    Updated Track Correlation
    Added Evaluation 
    Putting aside 10% of the playlist or 1 track for testing

--- src/CollaborativeFilter.java
+++ src/CollaborativeFilter.java
@@ -6,11 +6,12 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.ArrayUtils;
 
@@ -19,236 +20,127 @@ import org.apache.commons.lang.ArrayUtils;
  *
  */
 public class CollaborativeFilter {
+	private static final int MaximumK = 50;
+	private static final Random RANDOM = new Random();
 	static HashMap<String, HashSet<String>> playlistTrackMap;
 	static HashSet<String> trainTracks;
-	static HashMap<String, String> testMap;
-	static HashMap<String, Double> playlistsCorrelation;
-	static HashSet<String> testTracks;
+	static HashMap<String, HashSet<String>> testMap;
 	static HashMap<String, TreeMap<String, Double>> prediction;
-	static HashMap<String, HashMap<String, Integer>> playlistArtistMap;
-	static HashMap<String, HashMap<String, Integer>> playlistAlbumMap;
+	static HashMap<String, HashMap<String, Double>> playlistArtistMap;
+	static HashMap<String, HashMap<String, Double>> playlistAlbumMap;
+	static HashMap<String, Double> tracksCorrelation;
+	static HashMap<String, Double> albumsCorrelation;
+	static HashMap<String, Double> artistsCorrelation;
+	static double precision_5, precision_10, precision_20, precision_25, precision_50; 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String trainingTrackFilename = "C:\\Users\\Mythri Thippareddy\\Desktop\\Machine_Learning\\Project\\Spotify-Recsys-2018\\scripts\\mpd.track.slice.0-999.csv";
-		String trainingAlbumFilename = "C:\\Users\\Mythri Thippareddy\\Desktop\\Machine_Learning\\Project\\Spotify-Recsys-2018\\scripts\\mpd.album.slice.0-999.csv";
-		String trainingArtistFileName = "C:\\Users\\Mythri Thippareddy\\Desktop\\Machine_Learning\\Project\\Spotify-Recsys-2018\\scripts\\mpd.artist.slice.0-999.csv";
+		// Getting training file names
+		String trainingFilename = args[0];
+		
+		//Initializing all global the variables
 		playlistTrackMap = new HashMap<String, HashSet<String>>();
 		trainTracks = new HashSet<String>();
-		testMap = new HashMap<String, String>();
-		playlistsCorrelation = new HashMap<String, Double>();
-		testTracks = new HashSet<String>();
+		testMap = new HashMap<String, HashSet<String>>();
+		tracksCorrelation = new HashMap<String, Double>();
+		albumsCorrelation = new HashMap<String, Double>();
+		artistsCorrelation = new HashMap<String, Double>();
 		prediction = new HashMap<String, TreeMap<String, Double>>();
-
-		playlistArtistMap = new HashMap<String, HashMap<String, Integer>>();
-		hashPlaylistArtists(trainingArtistFileName);
-
-		playlistAlbumMap = new HashMap<String, HashMap<String, Integer>>();
-		hashPlaylistAlbums(trainingAlbumFilename);
-
-		hashPlaylistTracks(trainingTrackFilename);
-		testPlaylistTracks();
-	}
-
-	private static void hashPlaylistArtists(String trainingArtistFileName) {
-		BufferedReader br = null;
-		String line = "", cvsSplitBy = ",", playlistId;
-		String[] playlist;
-		HashMap<String, Integer> artistsHashMap;
-
-		try {
-			br = new BufferedReader(new FileReader(trainingArtistFileName));
-			while ((line = br.readLine()) != null) {
-				playlist = line.split(cvsSplitBy);
-				playlistId = playlist[0];
-				playlist = (String[]) ArrayUtils.removeElement(playlist, playlistId);
-				System.out.println("Number of Artists=" + playlist.length);
-				artistsHashMap = new HashMap<String, Integer>();
-				for (int i = 0; i < playlist.length; i++) {
-					if (artistsHashMap.containsKey(playlist[i])) {
-						artistsHashMap.put(playlist[i], artistsHashMap.get(playlist[i]) + 1);
-					} else {
-						artistsHashMap.put(playlist[i], 1);
-					}
-				}
-				// Normalizing the counts
-				for (String key : artistsHashMap.keySet()) {
-					artistsHashMap.put(key, artistsHashMap.get(key) / playlist.length);
-				}
-				playlistArtistMap.put(playlistId, artistsHashMap);
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void hashPlaylistAlbums(String trainingAlbumFilename) {
-		BufferedReader br = null;
-		String line = "", cvsSplitBy = ",", playlistId;
-		String[] playlist;
-		HashMap<String, Integer> albumsHashMap;
-
-		try {
-			br = new BufferedReader(new FileReader(trainingAlbumFilename));
-			while ((line = br.readLine()) != null) {
-				playlist = line.split(cvsSplitBy);
-				playlistId = playlist[0];
-				playlist = (String[]) ArrayUtils.removeElement(playlist, playlistId);
-				System.out.println("Number of Albums=" + playlist.length);
-				albumsHashMap = new HashMap<String, Integer>();
-				for (int i = 0; i < playlist.length; i++) {
-					if (albumsHashMap.containsKey(playlist[i])) {
-						albumsHashMap.put(playlist[i], albumsHashMap.get(playlist[i]) + 1);
-					} else {
-						albumsHashMap.put(playlist[i], 1);
-					}
-				}
-				// Normalizing the counts
-				for (String key : albumsHashMap.keySet()) {
-					albumsHashMap.put(key, albumsHashMap.get(key) / playlist.length);
-				}
-				playlistAlbumMap.put(playlistId, albumsHashMap);
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static TreeMap<String, Double> sortMapByValue(TreeMap<String, Double> recommendations) {
-		Comparator<String> comparator = new ValueComparator(recommendations);
-		TreeMap<String, Double> result = new TreeMap<String, Double>(comparator);
-		result.putAll(recommendations);
-		return result;
-	}
-
-	private static void testPlaylistTracks() {
-		// TODO Auto-generated method stub
-		Iterator<String> it = testMap.keySet().iterator();
-		String playlist1, originalTrack;
-		double k = 0.0, predictedValue;
-		TreeMap<String, Double> recommendations;
-		while (it.hasNext()) {
-			playlist1 = it.next();
-			originalTrack = testMap.get(playlist1);
-			recommendations = new TreeMap<String, Double>();
-			for (String track : trainTracks) {
-				predictedValue = 0.0;
-				for (String playlist2 : playlistTrackMap.keySet()) {
-					if (playlistTrackMap.get(playlist2).contains(track)) {
-						predictedValue += calculateCorrelation(playlist1, playlist2);
-					}
-				}
-				recommendations.put(track, predictedValue);
-			}
-			recommendations = sortMapByValue(recommendations);
-			prediction.put(playlist1, recommendations);
-			System.out.println(playlist1 + " - " + originalTrack + " -> " + recommendations.get(originalTrack) + "/"
-					+ recommendations.firstEntry().getValue());
-		}
-	}
-
-	private static double calculateCorrelation(String playlist1, String playlist2) {
-		// TODO Auto-generated method stub
-		String key;
-		if (playlist1.compareTo(playlist2) < 0) {
-			key = playlist1 + "_" + playlist2;
-		}
-		key = playlist2 + "_" + playlist1;
-		if (playlistsCorrelation.containsKey(key))
-			return playlistsCorrelation.get(key);
-		if (playlist1.equals("0") && playlist2.equals("405"))
-			System.out.println("0 & 405");
-		HashSet<String> commonTracks = (HashSet<String>) playlistTrackMap.get(playlist1).clone();
-		commonTracks.retainAll(playlistTrackMap.get(playlist2));
-		playlistsCorrelation.put(key, (double) commonTracks.size());
-		return commonTracks.size();
-	}
-
-	private static double correlationBetweenAlbums(String playlist1, String playlist2) {
-		double albumCorrelation = 0;
-		Set<String> commonAlbums = playlistAlbumMap.get(playlist1).keySet();
-		commonAlbums.retainAll(playlistAlbumMap.get(playlist2).keySet());
+		playlistArtistMap = new HashMap<String, HashMap<String, Double>>();
+		playlistAlbumMap = new HashMap<String, HashMap<String, Double>>();
 		
-		for (String album : commonAlbums) {
-				albumCorrelation += playlistAlbumMap.get(playlist1).get(album)
-						*playlistAlbumMap.get(playlist2).get(album);
-		}
-		return albumCorrelation;
-	}
-
-	private static double correlationBetweenArtists(String playlist1, String playlist2) {
-		double artistCorrelation = 0;
-		Set<String> commonArtists = playlistArtistMap.get(playlist1).keySet();
-		commonArtists.retainAll(playlistAlbumMap.get(playlist2).keySet());
+		//Reading all the training files into the hash maps
+		hashPlaylists(trainingFilename);
 		
-		for (String artist : commonArtists) {
-			artistCorrelation += playlistArtistMap.get(playlist1).get(artist)
-						*playlistArtistMap.get(playlist2).get(artist);
-		}
-		return artistCorrelation;
+		//Testing the playlists		
+		testPlaylists();
 	}
 
 	/**
-	 * Hash the Playlist and Track Data set from the training file
+	 * Loop over training files and create a artist, album and track map
 	 * 
-	 * @param filename
-	 * @param trainingArtistFileName
-	 * @param trainingAlbumFilename
+	 * @param trainingTrackFilename
 	 */
-	private static void hashPlaylistTracks(String trainingTrackFilename) {
-		// TODO Auto-generated method stub
+	private static void hashPlaylists(String trainingFilename) {
 		BufferedReader br = null;
-		String line = "";
-		String cvsSplitBy = ",", playlistId;
-		String[] playlist;
-		HashSet<String> tracks;
-
+		String line = "",  cvsSplitBy = ",", playlistId;
+		int playlistLength;
+		String[] row;
+		List<Integer> testTrackIndexes;
+		HashSet<String> testTracks, playlistTracks;
+		HashMap<String, Double> playlistArtists, playlistAlbums;
 		try {
-			br = new BufferedReader(new FileReader(trainingTrackFilename));
-			int counts = 0, rand;
-			String track, testTrack;
+			br = new BufferedReader(new FileReader(trainingFilename));
+			int rand;
+			// each line in csv is a playlist
 			while ((line = br.readLine()) != null) {
-				playlist = line.split(cvsSplitBy);
-				playlistId = playlist[0];
-				playlist = (String[]) ArrayUtils.removeElement(playlist, playlistId);
-				counts += playlist.length;
-				tracks = new HashSet<String>();
-				rand = new Random().nextInt(playlist.length);
-				testTrack = playlist[rand];
-				playlist = (String[]) ArrayUtils.removeElement(playlist, testTrack);
-				for (int i = 0; i < playlist.length; i++) {
-					tracks.add(playlist[i]);
+				//Get new row of excel
+				row = line.split(cvsSplitBy);
+				
+				//Get playlist ID from first column and remove it from row
+				playlistId = row[0];
+				row = (String[]) ArrayUtils.removeElement(row, playlistId);
+
+				//Put aside 10% of tracks from playlist for testing 
+				testTracks = new HashSet<String>();
+				playlistLength = row.length/3;
+				int noOfTestTracks =  (playlistLength > 10) ? (int) (playlistLength*0.1) : 1;
+				for(int i =0; i< noOfTestTracks; i++ ) {
+					int t = RANDOM.nextInt(playlistLength)*3;
+					if(row[t].contains("album") || row[t].contains("artist")) {
+						System.exit(0);
+					}
+					testTracks.add(row[t]);
+					row = (String[]) ArrayUtils.remove(row, t); //Removing the track
+					row = (String[]) ArrayUtils.remove(row, t); //Removing the artist
+					row = (String[]) ArrayUtils.remove(row, t); //Removing the album
+					playlistLength--;
 				}
-				playlistTrackMap.put(playlistId, tracks);
-				trainTracks.addAll(tracks);
-				testTracks.add(testTrack);
-				testMap.put(playlistId, testTrack);
+				
+				//Hash training items
+				playlistTracks = new HashSet<String>();
+				playlistArtists = new HashMap<String, Double>();
+				playlistAlbums = new HashMap<String, Double>();
+				for(int i=0 ; i< row.length;) {
+					playlistTracks.add(row[i++]);
+					if (playlistArtists.containsKey(row[i])) {
+						playlistArtists.put(row[i], playlistArtists.get(row[i])+1);
+					} else {
+						playlistArtists.put(row[i], 1.0);
+					}
+					if (playlistAlbums.containsKey(row[++i])) {
+						playlistAlbums.put(row[i], playlistAlbums.get(row[i])+1);
+					} else {
+						playlistAlbums.put(row[i], 1.0);
+					}
+					i++;
+				}
+				//Normalizing 
+				for (String key : playlistArtists.keySet()) {
+					playlistArtists.put(key, playlistArtists.get(key)/playlistTracks.size());
+				}
+				for (String key : playlistAlbums.keySet()) {
+					playlistAlbums.put(key, playlistAlbums.get(key)/playlistTracks.size());
+				}
+				playlistArtistMap.put(playlistId, playlistArtists);
+				playlistAlbumMap.put(playlistId, playlistAlbums);
+				playlistTrackMap.put(playlistId, playlistTracks);
+				trainTracks.addAll(playlistTracks);
+				testMap.put(playlistId, testTracks);
 			}
 			System.out.println("Number of Playlists: " + playlistTrackMap.size());
-			System.out.println("Number of tracks: " + counts);
 			System.out.println("Number of unique train tracks: " + trainTracks.size());
-			System.out.println("Number of unique test tracks: " + testTracks.size());
-			HashSet<String> temp = (HashSet<String>) testTracks.clone();
-			temp.removeAll(trainTracks);
-			System.out.println("Number of new test tracks: " + temp.size());
-			System.out.println("Number of test playlists: " + testMap.size());
-			// Drop Test cases with new Tracks;
-			Iterator it = ((HashMap<String, String>) testMap.clone()).keySet().iterator();
-			String key;
-			HashSet<String> tempSet;
-			while (it.hasNext()) {
-				key = (String) it.next();
-				if (temp.contains(testMap.get(key))) {
-					testMap.remove(key);
+			
+			// Drop Test Tracks which are not available in training tracks;
+			for(String pid : testMap.keySet()) {
+				testTracks = new HashSet<String>();
+				for(String test : testMap.get(pid)) {
+					if(!trainTracks.contains(test)) {
+						testTracks.add(test);
+					}
 				}
+//				System.out.println("Removing "+ testTracks.size() + " tracks out of "+ testMap.get(pid).size() +" from playlist "+ pid);
+				testMap.get(pid).removeAll(testTracks);
 			}
-			System.out.println("Number of test playlists: " + testMap.size());
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -263,6 +155,145 @@ public class CollaborativeFilter {
 			}
 		}
 	}
+	
+
+	public static TreeMap<String, Double> sortMapByValue(TreeMap<String, Double> recommendations) {
+		Comparator<String> comparator = new ValueComparator(recommendations);
+		TreeMap<String, Double> result = new TreeMap<String, Double>(comparator);
+		result.putAll(recommendations);
+		return result;
+	}
+
+	private static void testPlaylists() {
+		Iterator<String> it = testMap.keySet().iterator();
+		String playlist1;
+		Set<String> originalTracks;
+		double k = 0.0, predictedValue;
+		TreeMap<String, Double> recommendations;
+		// Iterating over all playlists in testMap 
+		while (it.hasNext()) {
+			playlist1 = it.next();
+			originalTracks = testMap.get(playlist1);
+			recommendations = new TreeMap<String, Double>();
+			for (String track : trainTracks) {
+				predictedValue = 0.0;
+				for (String playlist2 : playlistTrackMap.keySet()) {
+					predictedValue += correlation(playlist1, playlist2, playlistTrackMap.get(playlist2).contains(track));
+				}
+				recommendations.put(track, predictedValue);
+			}
+			recommendations = sortMapByValue(recommendations);
+			evaluate(originalTracks, recommendations);
+			prediction.put(playlist1, recommendations);
+		}
+		System.out.println("Precision @ 5 "+ precision_5/testMap.size());
+		System.out.println("Precision @ 10 "+ precision_10/testMap.size());
+		System.out.println("Precision @ 20 "+ precision_20/testMap.size());
+		System.out.println("Precision @ 25 "+ precision_25/testMap.size());
+		System.out.println("Precision @ 50 "+ precision_50/testMap.size());
+	}
+
+	
+	/**
+	 * Computes the Precision values
+	 * 
+	 * @param originalTracks
+	 * @param recommendations
+	 */
+	private static void evaluate(Set<String> originalTracks, TreeMap<String, Double> recommendations) {
+		int p =0 ;
+		for(int i=0; i < MaximumK; i++) {
+			Entry<String, Double> recommendation = recommendations.pollFirstEntry();
+			if(originalTracks.contains(recommendation.getKey())) {
+				p++;
+				switch (i) {
+				case 5:
+					precision_5 += (double) p/i;
+					break;
+				case 10:
+					precision_10 += (double) p/i;
+					break;
+				case 20:
+					precision_20 += (double) p/i;
+					break;
+				case 25:
+					precision_25 += (double) p/i;
+					break;
+				case 50:
+					precision_50 += (double) p/i;
+					break;
+				default:
+					break;
+				}
+		    }
+		}
+	}
+
+	private static double correlation(String playlist1, String playlist2, boolean contains) {
+		// TODO Weighted correlations
+		if(contains)
+			return tracksCorrelation(playlist1, playlist2);
+		return 0;
+	}
+
+	private static double tracksCorrelation(String playlist1, String playlist2) {
+		String key = generateKey(playlist1, playlist2);
+		//Return from memory if available
+		if (!tracksCorrelation.containsKey(key)) {
+			//Calculate track correlation
+			HashSet<String> commonTracks = (HashSet<String>) playlistTrackMap.get(playlist1).clone();
+			commonTracks.retainAll(playlistTrackMap.get(playlist2));
+			//correlation = (|commontracks|)^2/ (|playlist1|*|playlist2|)
+			double correlation = (double) (commonTracks.size()*commonTracks.size())/(playlistTrackMap.get(playlist1).size()*playlistTrackMap.get(playlist2).size());
+			tracksCorrelation.put(key, correlation);
+		}
+		return tracksCorrelation.get(key);
+	}
+
+	private static String generateKey(String playlist1, String playlist2) {
+		String key;
+		if (playlist1.compareTo(playlist2) < 0) {
+			key = playlist1 + "_" + playlist2;
+		}
+		key = playlist2 + "_" + playlist1;
+		return key;
+	}
+
+	private static double albumsCorrelation(String playlist1, String playlist2) {
+		String key = generateKey(playlist1, playlist2);
+		//Return from memory if available
+		if (!albumsCorrelation.containsKey(key)) {
+			//Calculate Album Correlation
+			double correlation = 0;
+			Set<String> commonAlbums = playlistAlbumMap.get(playlist1).keySet();
+			commonAlbums.retainAll(playlistAlbumMap.get(playlist2).keySet());
+			
+			for (String album : commonAlbums) {
+					correlation += playlistAlbumMap.get(playlist1).get(album)
+							*playlistAlbumMap.get(playlist2).get(album);
+			}
+			albumsCorrelation.put(key, correlation);
+		}
+		return albumsCorrelation.get(key);
+	}
+
+	private static double artistsCorrelation(String playlist1, String playlist2) {
+		String key = generateKey(playlist1, playlist2);
+		//Return from memory if available
+		if (!artistsCorrelation.containsKey(key)) {
+			//Calculate Album Correlation
+			double correlation = 0;
+			Set<String> commonArtists = playlistArtistMap.get(playlist1).keySet();
+			commonArtists.retainAll(playlistAlbumMap.get(playlist2).keySet());
+			
+			for (String artist : commonArtists) {
+				correlation += playlistArtistMap.get(playlist1).get(artist)
+							*playlistArtistMap.get(playlist2).get(artist);
+			}
+			artistsCorrelation.put(key, correlation);
+		}
+		return artistsCorrelation.get(key);
+	}
 
 }
 
@@ -276,6 +307,14 @@ class ValueComparator implements Comparator {
 	public int compare(Object keyA, Object keyB) {
 		Comparable valueA = (Comparable) map.get(keyA);
 		Comparable valueB = (Comparable) map.get(keyB);
-		return valueB.compareTo(valueA);
+		int x = valueB.compareTo(valueA);
+		if(x == 0) {
+			keyA = (String) keyA;
+			keyB = (String) keyB;
+			if(!keyA.equals(keyB)) {
+				return 1;
+			}
+		}
+		return x;
 	}
 }
